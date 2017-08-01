3d3f60d67d51e3cba85312682e015e68563854ea
Finley <altimagewritingco@gmail.com>
2017-08-02 01:55:13 +0200

    Update DrOtto to BakerBob
    rename, reduce minimum_bid, add maximum_bid

--- test/support/config_test.rb
+++ test/support/config_test.rb
@@ -1,19 +1,20 @@
 require 'test_helper'
 
-module DrOtto
-  class ConfigTest < DrOtto::Test
+module BakerBob
+  class ConfigTest < BakerBob::Test
     include Config
     
     def setup
       override_config(
-        drotto: {
+        bakerbob: {
           block_mode: 'irreversible',
           account_name: 'social',
           posting_wif: '5JrvPrQeBBvCRdjv29iDvkwn3EQYZ9jqfAHzrCyUvfbEbRkrYFC',
           active_wif: '5JrvPrQeBBvCRdjv29iDvkwn3EQYZ9jqfAHzrCyUvfbEbRkrYFC',
           batch_vote_weight: '3.13 %',
           reserve_vote_weight: '0.01 %',
-          minimum_bid: '2.000 SBD',
+          minimum_bid: '0.100 SBD',
+          maximum_bid: '0.500 SBD',
           blacklist: 'mikethemug'
         }, chain_options: {
           chain: 'steem',
@@ -32,6 +33,14 @@ module DrOtto
     
     def test_minimum_bid_asset
       assert minimum_bid_asset
+    end
+
+    def test_maximum_bid_amount
+      assert maximum_bid_amount
+    end
+    
+    def test_maximum_bid_asset
+      assert maximum_bid_asset
     end
   end
 end
