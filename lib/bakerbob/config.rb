aa1a9bee45003f85d87f8e7248f7df97557e9e99
Finley <altimagewritingco@gmail.com>
2017-08-02 03:10:57 +0200

    rename drotto to bakerbob, add maximum_bid

--- lib/bakerbob/config.rb
+++ lib/bakerbob/config.rb
@@ -1,8 +1,8 @@
-module DrOtto
+module BakerBob
   module Config
     MAX_BASE_BLOCK_SPAN = 2880
     
-    DEFAULT_LOGGER = Logger.new('drotto.log')
+    DEFAULT_LOGGER = Logger.new('bakerbob.log')
     
     DEFAULT_CHAIN_OPTIONS = {
       logger: DEFAULT_LOGGER
@@ -26,35 +26,35 @@ module DrOtto
     end
     
     def block_mode
-      ENV['DROTTO_BLOCK_MODE'] || config[:drotto][:block_mode]
+      ENV['BAKERBOB_BLOCK_MODE'] || config[:bakerbob][:block_mode]
     end
     
     def account_name
-      ENV['DROTTO_ACCOUNT_NAME'] || config[:drotto][:account_name]
+      ENV['BAKERBOB_ACCOUNT_NAME'] || config[:bakerbob][:account_name]
     end
     
     def posting_wif
-      ENV['DROTTO_POSTING_WIF'] || config[:drotto][:posting_wif]
+      ENV['BAKERBOB_POSTING_WIF'] || config[:bakerbob][:posting_wif]
     end
     
     def active_wif
-      ENV['DROTTO_ACTIVE_WIF'] || config[:drotto][:active_wif]
+      ENV['BAKERBOB_ACTIVE_WIF'] || config[:bakerbob][:active_wif]
     end
     
     def min_effective_weight
-      ((ENV['DROTTO_MIN_EFFECTIVE_WEIGHT'] || config[:drotto][:min_effective_weight]).to_f * 100).to_i
+      ((ENV['BAKERBOB_MIN_EFFECTIVE_WEIGHT'] || config[:bakerbob][:min_effective_weight]).to_f * 100).to_i
     end
     
     def batch_vote_weight
-      (ENV['DROTTO_BATCH_VOTE_WEIGHT'] || (config[:drotto][:batch_vote_weight]).to_f * 100).to_i
+      (ENV['BAKERBOB_BATCH_VOTE_WEIGHT'] || (config[:bakerbob][:batch_vote_weight]).to_f * 100).to_i
     end
     
     def reserve_vote_weight
-      ((ENV['DROTTO_RESERVE_VOTE_WEIGHT'] || config[:drotto][:reserve_vote_weight]).to_f * 100).to_i
+      ((ENV['BAKERBOB_RESERVE_VOTE_WEIGHT'] || config[:bakerbob][:reserve_vote_weight]).to_f * 100).to_i
     end
     
     def minimum_bid
-      ENV['DROTTO_MINIMUM_BID'] || config[:drotto][:minimum_bid]
+      ENV['BAKERBOB_MINIMUM_BID'] || config[:bakerbob][:minimum_bid]
     end
     
     def minimum_bid_amount
@@ -65,16 +65,28 @@ module DrOtto
       minimum_bid.split(' ').last
     end
     
+    def maximum_bid
+      ENV['BAKERBOB_MINIMUM_BID'] || config[:bakerbob][:maximum_bid]
+    end
+    
+    def maximum_bid_amount
+      maximum_bid.split(' ').first.to_f
+    end
+    
+    def maximum_bid_asset
+      maximum_bid.split(' ').last
+    end
+
     def blacklist
-      (ENV['DROTTO_BLACKLIST'] || config[:drotto][:blacklist]).to_s.downcase.split(' ')
+      (ENV['BAKERBOB_BLACKLIST'] || config[:bakerbob][:blacklist]).to_s.downcase.split(' ')
     end
     
     def chain_options
       chain_options = config[:chain_options].merge(DEFAULT_CHAIN_OPTIONS)
       
-      chain = ENV['DROTTO_CHAIN_OPTIONS_CHAIN']
+      chain = ENV['BAKERBOB_CHAIN_OPTIONS_CHAIN']
       chain_options = chain_options.merge(chain: chain.to_s) if !!chain
-      url = ENV['DROTTO_CHAIN_OPTIONS_URL']
+      url = ENV['BAKERBOB_CHAIN_OPTIONS_URL']
       chain_options = chain_options.merge(url: url) if !!url
       
       chain_options.dup
